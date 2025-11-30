515ff2c6e1676e92ebcc62af8aede1ad63be1196
Mustafa Elbaz <m9stafa05@gmail.com>
2025-12-01 01:12:36 +0200

    Build The quizzes screen ui

--- lib/modules/student/features/quizzes/ui/quizzes_screen.dart
+++ lib/modules/student/features/quizzes/ui/quizzes_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:zrc/modules/student/core/widgets/custom_app_bar.dart';
+import 'package:zrc/modules/student/features/quizzes/ui/widgets/quizzes_filter_row.dart';
+import 'package:zrc/modules/student/features/quizzes/ui/widgets/quizzes_list_view.dart';
 
-class QuizzesScreen extends StatelessWidget {
+class QuizzesScreen extends StatefulWidget {
   const QuizzesScreen({super.key});
 
   @override
+  State<QuizzesScreen> createState() => _QuizzesScreenState();
+}
+
+class _QuizzesScreenState extends State<QuizzesScreen> {
+  String selectedFilter = 'All';
+
+  void onFilterChanged(String newFilter) {
+    setState(() {
+      selectedFilter = newFilter;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Center(child: Text("Materials Screen")));
+    return SafeArea(
+      child: Column(
+        children: [
+          const CustomAppBar(title: "Quizzes"),
+
+          // Filter Tabs
+          QuizzesFilterRow(
+            selectedFilter: selectedFilter,
+            onFilterChanged: onFilterChanged,
+          ),
+
+          // Quizzes List
+          Expanded(child: QuizzesListView(filter: selectedFilter)),
+        ],
+      ),
+    );
   }
 }
