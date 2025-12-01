ebf02285409e2722539639467c2db894642e3221
Mustafa Elbaz <m9stafa05@gmail.com>
2025-12-01 03:21:49 +0200

    Build description and instructor card

---
+++ lib/modules/student/features/quizzes/ui/widgets/quiz_detailed/description_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:zrc/core/themes/app_text_styles.dart';
+import 'package:zrc/core/utils/spacing.dart';
+import 'package:zrc/modules/student/features/quizzes/data/model/quiz_model.dart';
+
+class DescriptionCard extends StatelessWidget {
+  final QuizModel quiz;
+
+  const DescriptionCard({super.key, required this.quiz});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16.w),
+      padding: EdgeInsets.all(20.w),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha((0.04 * 255).toInt()),
+            blurRadius: 8,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Icon(
+                Icons.description_outlined,
+                size: 20.sp,
+                color: Colors.blue[700],
+              ),
+              horizontalSpacing(8),
+              Text('Description', style: AppTextStyles.font16BlackBold),
+            ],
+          ),
+          verticalSpacing(12),
+          Text(quiz.description, style: AppTextStyles.font13greyRegular),
+        ],
+      ),
+    );
+  }
+}
