3fdfd7ea4b8dad6587f9991ac32a00336d29a79b
Mustafa Elbaz <m9stafa05@gmail.com>
2025-12-03 03:26:51 +0200

    Add New Function For Translating NAmes To english

--- lib/core/utils/functions/change_language.dart
+++ lib/core/utils/functions/change_language.dart
@@ -1,3 +1,4 @@
+import 'package:arabic_roman_conv/arabic_roman_conv.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
@@ -11,4 +12,16 @@ void changeLanguage(BuildContext context) {
     context.setLocale(const Locale('en'));
   }
   (context as Element).markNeedsBuild();
+}
+
+String changeNameToEn(BuildContext context, String nameAr) {
+  if (context.locale.languageCode == 'en') {
+    // Create an instance of ArabicRomanConv
+    final converter = ArabicRomanConv();
+    final nameEn = converter.romanized(nameAr);
+    return nameEn;
+  } else {
+    // Keep Arabic
+    return nameAr;
+  }
 }
