fe5e4f4416e8c3dacd79e63c91164944907a0421
mustafaelbaz5 <m9stafa05@gmail.com>
2025-12-28 00:39:15 +0200

    Rebuild switch lan fun to fix the delay in the rebuild

--- lib/core/utils/functions/app_setting_fun.dart
+++ lib/core/utils/functions/app_setting_fun.dart
@@ -9,16 +9,14 @@ import '../../extensions/context_extensions.dart';
 import '../../themes/cubit/theme_cubit.dart';
 
 /// Switch Language between Arabic and English
-void switchLanguage(final BuildContext context) {
-  final Locale current = context.locale;
+Future<void> switchLanguage(final BuildContext context) async {
+  final current = context.locale;
 
-  // Switch locale
   if (current.languageCode == 'en') {
-    context.setLocale(const Locale('ar'));
+    await context.setLocale(const Locale('ar'));
   } else {
-    context.setLocale(const Locale('en'));
+    await context.setLocale(const Locale('en'));
   }
-  (context as Element).markNeedsBuild();
 }
 
 /// Switch Theme between Light and Dark
