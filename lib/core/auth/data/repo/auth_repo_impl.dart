efb27c85aae40114f08d4a047b4ecb00331481c9
mustafaelbaz5 <m9stafa05@gmail.com>
2025-12-29 15:35:26 +0200

    Add Json key , supa base anonkey and supabaseUrl to constants

--- lib/core/auth/data/repo/auth_repo_impl.dart
+++ lib/core/auth/data/repo/auth_repo_impl.dart
@@ -1,6 +1,7 @@
 import 'package:zrc/core/auth/data/model/user_model.dart';
 import 'package:zrc/core/auth/data/repo/auth_repo.dart';
 import 'package:zrc/core/auth/data/service/auth_service.dart';
+import 'package:zrc/core/config/constants.dart';
 import 'package:zrc/core/error/models/app_error.dart';
 import 'package:zrc/core/error/types/error_handler.dart';
 import 'package:zrc/core/storage/secure_storage.dart';
@@ -27,7 +28,7 @@ class AuthRepoImpl implements AuthRepo {
       );
 
       await _secureStorage.saveString(
-        key: 'current_user',
+        key: Constants.userStorageKey,
         value: user.toJsonString(),
       );
 
@@ -42,7 +43,7 @@ class AuthRepoImpl implements AuthRepo {
   Future<UserModel?> getCurrentUser() async {
     try {
       final String? jsonString = await _secureStorage.getString(
-        key: 'current_user',
+        key: Constants.userStorageKey,
       );
       if (jsonString == null || jsonString.isEmpty) return null;
       return UserModel.fromJsonString(jsonString);
@@ -54,6 +55,6 @@ class AuthRepoImpl implements AuthRepo {
 
   @override
   Future<void> logout() async {
-    await _secureStorage.clear(key: 'current_user');
+    await _secureStorage.clear(key: Constants.userStorageKey);
   }
 }
